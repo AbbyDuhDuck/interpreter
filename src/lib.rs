0a5a928d889e016a469bcfd62966073c82919d13
AbbyDuhDuck <abbyduck96@gmail.com>
2024-03-22 19:43:56 -0400

    Create lib.rs
    make library file

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! # Interpreter Tutorial
+//! 
+//! Something something, basic interpreter in rust. 
+
+pub mod io;
+
+pub fn exec(expr_str: &str) {
+    println!("Some output from expr {expr_str:?}");
+}
