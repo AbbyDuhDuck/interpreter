08e7c8b657b78eac882b34cf5f7b34dc43817835
AbbyDuhDuck <abbyduck96@gmail.com>
2024-03-30 15:23:03 -0400

    Compacted
    removed necessary code

--- src/lang/math/mod.rs
+++ src/lang/math/mod.rs
@@ -8,15 +8,14 @@ pub use math::*;
 pub mod math {
     use super::parser::Parser;
     use super::lexer::Lexer;
-
-    use super::syntax::Expression;
+    
     use super::syntax::Expression::*;
     use once_cell::sync::Lazy;
 
     pub const LEXER: Lazy<Lexer> = Lazy::new(|| {
         let mut lexer = Lexer::new();
         let _ = lexer.define("op", "\\+|\\-|\\*|\\/|\\(|\\)");
-        // let _ = lexer.define("float", "[0-9]+\\.[0-9]+");
+        let _ = lexer.define("float", "[0-9]+\\.[0-9]+");
         let _ = lexer.define("int", "[0-9]+");
         let _ = lexer.define("ident", "[a-zA-Z_]+");
         lexer
@@ -24,41 +23,29 @@ pub mod math {
 
     pub const PARSER: Lazy<Parser> = Lazy::new(|| {
         let mut parser = Parser::new();
-        let _ = parser.define("EXPR", EXPR.to_owned());
-        let _ = parser.define("MATH:EXPR", MATH_EXPR.to_owned());
-        let _ = parser.define("TERM", TERM.to_owned());
-        let _ = parser.define("FACTOR", FACTOR.to_owned());
-        let _ = parser.define("NUM", NUM.to_owned());
-        let _ = parser.define("VAR", VAR.to_owned());
+        let _ = parser.define("EXPR", Expr("MATH:EXPR"));
+        let _ = parser.define("MATH:EXPR", ExprOr(vec![
+            SubExpr(vec![ Expr("TERM"), Token("op", "+"), Expr("MATH:EXPR") ]),
+            SubExpr(vec![ Expr("TERM"), Token("op", "-"), Expr("MATH:EXPR") ]),
+            Expr("TERM"),
+        ]));
+        let _ = parser.define("TERM", ExprOr(vec![
+            SubExpr(vec![ Expr("FACTOR"), Token("op", "*"), Expr("TERM") ]),
+            SubExpr(vec![ Expr("FACTOR"), Token("op", "/"), Expr("TERM") ]),
+            Expr("FACTOR"),
+        ]));
+        let _ = parser.define("FACTOR", ExprOr(vec![
+            SubExpr(vec![ Token("op", "("), Expr("VAR"), Token("op", ")")]),
+            SubExpr(vec![ Token("op", "("), Expr("MATH:EXPR"), Token("op", ")")]),
+            Expr("NUM"),
+            Expr("VAR"),
+        ]));
+        let _ = parser.define("NUM", ExprOr(vec![
+            Token("float", ""),
+            Token("int", ""),
+        ]));
+        let _ = parser.define("VAR", Token("ident", ""));
         parser
     });
-    
-    pub static EXPR: Expression = Expr("MATH:EXPR");
-
-    pub static MATH_EXPR: Lazy<Expression> = Lazy::new(|| ExprOr(vec![
-        SubExpr(vec![ Expr("TERM"), Token("op", "+"), Expr("MATH:EXPR") ]),
-        SubExpr(vec![ Expr("TERM"), Token("op", "-"), Expr("MATH:EXPR") ]),
-        Expr("TERM"),
-    ]));
-    
-    pub static TERM: Lazy<Expression> = Lazy::new(|| ExprOr(vec![
-        SubExpr(vec![ Expr("FACTOR"), Token("op", "*"), Expr("TERM") ]),
-        SubExpr(vec![ Expr("FACTOR"), Token("op", "/"), Expr("TERM") ]),
-        Expr("FACTOR"),
-    ]));
-    
-    pub static FACTOR: Lazy<Expression> = Lazy::new(|| ExprOr(vec![
-        SubExpr(vec![ Token("op", "("), Expr("VAR"), Token("op", ")")]),
-        SubExpr(vec![ Token("op", "("), Expr("MATH:EXPR"), Token("op", ")")]),
-        Expr("NUM"),
-        Expr("VAR"),
-    ]));
-    
-    pub static NUM: Lazy<Expression> = Lazy::new(|| ExprOr(vec![
-        // Token("float", ""),
-        Token("int", ""),
-    ]));
-    
-    pub static VAR: Expression = Token("ident", "");
 }
 
